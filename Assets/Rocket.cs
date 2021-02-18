b38d9ba4b5c0a3c86c6d060d41b0cf7747c60e59
beshu78910 <57365120+beshu78910@users.noreply.github.com>
2021-02-18 01:05:35 -0500

    Audio and Movement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,44 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+     // AudioClip sound;
      Rigidbody rigidbody;
+
+     private AudioSource audioSource;
+     
+     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     ProcessInput();
+
+        Thurst();
+     Rotate();
+
+     // audioSource.clip = sound;
+     // sound = Resources.Load<AudioClip>("rrocket");
         
     }
 
-    private void ProcessInput()
+ 
+      
+
+void Rotate()
+{
+    rigidbody.freezeRotation = true;
+    if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) == false)
+    {
+        print("Rotating Left");
+        transform.Rotate(Vector3.forward);
+            
+    } else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) == false)
+    {
+        print("Rotating Right");
+        transform.Rotate(-Vector3.forward);
+    }
+
+}
+
+    void Thurst()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             print("Thurst");
             rigidbody.AddRelativeForce(Vector3.up);
-            
-            
-        }
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) == false)
-        {
-            print("Rotating Left");
-            rigidbody.AddRelativeForce(Vector3.left);
-            
-        } else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) == false)
-        {
-            print("Rotating Right");
-            rigidbody.AddRelativeForce(Vector3.right);
-        }
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+                
+            } else if (!Input.GetKey(KeyCode.Space))
+            {
+                audioSource.Stop();
+            }
+        } 
         
 
     }
+
 }
