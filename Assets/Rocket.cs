1ccef839652218c7ccc6a3e0adba2e3d921241b4
beshu78910 <57365120+beshu78910@users.noreply.github.com>
2021-03-03 11:14:56 -0500

    Update levels

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
      // AudioClip sound;
      Rigidbody rigidbody;
+     [SerializeField] float RotationSpeed = 100f;
+     [SerializeField] float MainThurst = 100f;
+     private bool controlEnable = true;
+     
+
+     enum State
+     {
+         Alive,
+         Transcending,
+         Dying
+         
+     };
+
+     private State state = State.Alive;
 
      private AudioSource audioSource;
      
@@ -21,44 +36,97 @@ public class Rocket : MonoBehaviour
 
 
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadScene", 1f);
+                break;
+            default:
+                controlEnable = false;
+                print("Dead");
+                state = State.Dying;
+                audioSource.Stop();
+                Invoke("LoadSceneN0", 2f);
+                break;
+            
+        }
+    }
 
-    // Update is called once per frame
+     private void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+     private void LoadSceneN0()
+     {
+         SceneManager.LoadScene(0);
+     }
+
+     // Update is called once per frame
     void Update()
     {
+        if (state == State.Alive) 
+        {
+            Thurst();
+            Rotate();
 
-        Thurst();
-     Rotate();
-
+        }
+       
      // audioSource.clip = sound;
      // sound = Resources.Load<AudioClip>("rrocket");
         
     }
-
- 
+    
       
 
 void Rotate()
 {
     rigidbody.freezeRotation = true;
+   
+    float RotationTime = RotationSpeed * Time.deltaTime;
+
     if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) == false)
     {
         print("Rotating Left");
-        transform.Rotate(Vector3.forward);
+        
+        transform.Rotate(Vector3.forward * RotationTime);
+        
+        
+        
             
     } else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) == false)
     {
         print("Rotating Right");
-        transform.Rotate(-Vector3.forward);
+        transform.Rotate(-Vector3.forward * RotationTime);
     }
+
+    rigidbody.freezeRotation = false;
 
 }
 
+
+
     void Thurst()
     {
+        
+        float ThurstSpeed = MainThurst * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
+            
             print("Thurst");
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * ThurstSpeed);
+            
+            
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -69,7 +137,7 @@ void Rotate()
             }
         } 
         
-
+        
     }
 
 }
